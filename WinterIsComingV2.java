ab3df737f5fe2a79397ce95b8ba0b84418c96bee
Jan Ellenberger <jellenberger1999@gmail.com>
2021-10-01 16:39:44 +0200

    2nd commit
    2nd commit

---
+++ WinterIsComingV2.java
@@ -0,0 +1,25 @@
+package kap1;
+
+/*
+*******************
+* WinterIsComingV2
+* Jara Zihlmann
+* Vithusan Ramalingam
+* Jan Ellenberger
+*********************
+*/
+public class WinterIsComingV2 {
+
+    public static void main(String[] args) {
+        
+
+        System.out.print("Winter\n" + "is\n" + "coming\n");
+        
+
+
+
+
+    }
+
+    
+}
