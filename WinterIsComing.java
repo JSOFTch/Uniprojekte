8d6c14c5856e32c64665423c530cab5cbe8aa094
Jan Ellenberger <43755111+JSOFTch@users.noreply.github.com>
2021-10-03 15:39:03 +0200

    5th
    5th

---
+++ WinterIsComing.java
@@ -0,0 +1,22 @@
+package kap1;
+
+/*
+*****************
+*WinterIsComing
+*
+*Jara Zihlman (20-117-032)
+*Vithusan Ramalingam (21-105-515)
+*Jan Ellenberger (21-103-643)
+*
+*****************
+*/
+
+public class WinterIsComing {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Winter is coming");
+
+    }
+    
+}
